c570c004d53aa5319fa484695e7497c0a8bb1af1
Louis <louisitvn@gmail.com>
2016-09-30 17:29:33 +0000

    Update

--- populate.rb
+++ populate.rb
@@ -1,5 +1,5 @@
-$prefix = 'mx_'
-$campaign_id = 7
+$prefix = 'acelle_'
+
 
 require 'sqlite3'
 require 'rubygems'
@@ -21,6 +21,10 @@ class RouletteWheel
     }
   end
 
+  def self.likely(likelihood)
+    return rand < likelihood
+  end
+
   def self.with(possibility)
     return yield if rand < possibility
   end
@@ -31,9 +35,9 @@ ActiveRecord::Base.establish_connection(
   adapter: 'mysql2',
   encoding: 'utf8',
   host: 'localhost',
-  database: 'mailixa',
-  username: 'mailixa',
-  password: 'mailixa',
+  database: 'acelle_demo',
+  username: 'acelle_demo',
+  password: 'acelle_demo',
   port: 3306,
   timeout: 15000 # You'll only get the BusyException if it takes longer than 15 seconds.
 )
@@ -91,54 +95,14 @@ $agents = ["Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like
 $ips = IpLocation::all.map(&:ip_address)
 $links = [{url: 'http://sales.sample.com/promotion'}, {url: 'http://sales.sample.com/search=new'}]
 
-# Generate 200 subscriber for the List
-campaign = Campaign.find($campaign_id)
-campaign.update_attributes status: 'done'
-
+# clean up master data
 Link.delete_all
 CampaignLink.delete_all
 IpLocation.delete_all
-campaign.tracking_logs.destroy_all
-campaign.mail_list.subscribers.destroy_all
-
-# generate subscriber
-campaign.mail_list.subscribers.create(105.times.map{ |i| 
-  {
-    uid: SecureRandom.hex(10),
-    email: "user_#{i}@gmail.com",
-    status: RouletteWheel::generate('subscribed' => 81, 'unsubscribed' => 12, 'spam-reported' => 2, 'blacklisted' => 5),
-    from: 'unknown',
-    ip: '0.0.0.0',
-    subscriber_fields_attributes: [
-      { field_id: 2, value: "User #{i}"}, {field_id: 3, value: "User #{i}" }, {field_id: 1, value: "user_#{i}@gmail.com",}
-    ]
-  }
-})
-
-# generate tracking_logs
-campaign.tracking_logs.create(campaign.mail_list.subscribers.where(status: 'subscribed').map{|r|
-  message_id = SecureRandom.hex(10)
-  { subscriber_id: r.id, sending_server_id: SendingServer::first.id, user_id: campaign.user_id, message_id: message_id, runtime_message_id: message_id, status: RouletteWheel::generate('sent' => 91, 'failed' => 9) }
-})
-
-# generate tracking_logs
-campaign.tracking_logs.create(campaign.mail_list.subscribers.where("status IN (?)", ['spam-reported', 'blacklisted', 'unsubscribed']).map{|r|
-  message_id = SecureRandom.hex(10)
-  { subscriber_id: r.id, sending_server_id: SendingServer::first.id, user_id: campaign.user_id, message_id: message_id, runtime_message_id: message_id, status: 'sent' }
-})
-
-# generate feedback_logs
-FeedbackLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: 'spam-reported'}).joins(:subscriber).map{|r|
-  { message_id: r.message_id, runtime_message_id: r.runtime_message_id, feedback_type: 'spam', raw_feedback_content: '' }
-})
-
-# generate bounce_logs
-BounceLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: 'blacklisted'}).joins(:subscriber).map{|r|
-  { message_id: r.message_id, runtime_message_id: r.runtime_message_id, bounce_type: 'spam', raw: '' }
-})
 
 # populate the ip_locations table
 IpLocation.create(CSV.read('ipdb.csv', headers: true).map{|r|
+  #selected_ip = r['ip_from'].to_i + rand(r['ip_to'].to_i - r['ip_from'].to_i)
   {
     ip_address: IPAddr.new(r['ip_from'].to_i, Socket::AF_INET).to_s, 
     country_code: r['country_code'],
@@ -154,38 +118,104 @@ IpLocation.create(CSV.read('ipdb.csv', headers: true).map{|r|
   }
 })
 
-
+IpLocation.all.each do |r|
+  selected = RouletteWheel::generate(
+    {code: 'US', name: 'United States'} => 30,
+    {code: 'JP', name: 'Japan'} => 10,
+    {code: 'CN', name: 'China'} => 10,
+    {code: r.country_code, name: r.country_name} => 50
+  )
+  r.country_code = selected[:code]
+  r.country_name = selected[:name]
+  r.save
+end
 Link.create($links)
 
-Link.all.each do |link|
-  CampaignLink.create([
-    { link_id: link.id, campaign_id: campaign.id }
-  ])
-end
 
-# open logs
-OpenLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: ['unsubscribed', 'spam-reported']}).joins(:subscriber).map{|r|
-  date = rand(24).hours.ago
-  { message_id: r.message_id, ip_address: $ips.sample, user_agent: $agents.sample, created_at: date, updated_at: date }
-})
+# For every campaign
+campaigns = Campaign.where(status: ['ready', 'done'])
+campaigns.each do |campaign|
+  puts "Working on campaign [#{campaign.name}]"
+  
+  campaign.tracking_logs.destroy_all
+  campaign.mail_list.subscribers.destroy_all
 
-OpenLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: ['subscribed']}).joins(:subscriber).map{|r|
-  RouletteWheel::with(0.6) do 
+  Link.all.each do |link|
+    CampaignLink.create([
+      { link_id: link.id, campaign_id: campaign.id }
+    ])
+  end
+
+  # generate subscriber
+  campaign.mail_list.subscribers.create((rand(100) + 100).times.map{ |i| 
+    date = rand(4).months.ago
+    {
+      uid: SecureRandom.hex(10),
+      email: "user_#{i}@gmail.com",
+      status: RouletteWheel::generate('subscribed' => 81, 'unsubscribed' => 12, 'spam-reported' => 2, 'blacklisted' => 5),
+      from: 'unknown',
+      ip: '0.0.0.0',
+      created_at: date,
+      updated_at: date,
+      subscriber_fields_attributes: [
+        { field_id: 2, value: "User #{i}"}, {field_id: 3, value: "User #{i}" }, {field_id: 1, value: "user_#{i}@gmail.com", created_at: date, updated_at: date}
+      ]
+    }
+  })
+
+  # generate tracking_logs
+  campaign.tracking_logs.create(campaign.mail_list.subscribers.where(status: 'subscribed').map{|r|
+    message_id = SecureRandom.hex(10)
+    { subscriber_id: r.id, sending_server_id: SendingServer::first.id, user_id: campaign.user_id, message_id: message_id, runtime_message_id: message_id, status: RouletteWheel::generate('sent' => 91, 'failed' => 9) }
+  })
+
+  # generate tracking_logs
+  campaign.tracking_logs.create(campaign.mail_list.subscribers.where("status IN (?)", ['spam-reported', 'blacklisted', 'unsubscribed']).map{|r|
+    message_id = SecureRandom.hex(10)
+    { subscriber_id: r.id, sending_server_id: SendingServer::first.id, user_id: campaign.user_id, message_id: message_id, runtime_message_id: message_id, status: 'sent' }
+  })
+
+  # generate feedback_logs
+  FeedbackLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: 'spam-reported'}).joins(:subscriber).map{|r|
+    { message_id: r.message_id, runtime_message_id: r.runtime_message_id, feedback_type: 'spam', raw_feedback_content: '' }
+  })
+
+  # generate bounce_logs
+  BounceLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: 'blacklisted'}).joins(:subscriber).map{|r|
+    { message_id: r.message_id, runtime_message_id: r.runtime_message_id, bounce_type: 'spam', raw: '' }
+  })
+
+
+
+  # open logs
+  OpenLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: ['unsubscribed', 'spam-reported']}).joins(:subscriber).map{|r|
     date = rand(24).hours.ago
     { message_id: r.message_id, ip_address: $ips.sample, user_agent: $agents.sample, created_at: date, updated_at: date }
-  end
-}.reject(&:nil?))
+  })
 
-# open logs
-UnsubscribeLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: ['unsubscribed']}).joins(:subscriber).map{|r|
-  date = rand(24).hours.ago
-  { message_id: r.message_id, ip_address: $ips.sample, user_agent: $agents.sample, created_at: date, updated_at: date }
-})
+  OpenLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: ['subscribed']}).joins(:subscriber).map{|r|
+    RouletteWheel::with(0.6) do 
+      date = rand(24).hours.ago
+      { message_id: r.message_id, ip_address: $ips.sample, user_agent: $agents.sample, created_at: date, updated_at: date }
+    end
+  }.reject(&:nil?))
 
-# open logs
-ClickLog.create(campaign.tracking_logs.joins(:open_logs).select($prefix + 'tracking_logs.*', $prefix + 'open_logs.*').map{|r|
-  RouletteWheel::with(0.7) do 
+  # open logs
+  UnsubscribeLog.create(campaign.tracking_logs.where($prefix + 'subscribers' => {status: ['unsubscribed']}).joins(:subscriber).map{|r|
     date = rand(24).hours.ago
-    { message_id: r.message_id, url: $links.sample[:url], ip_address: r.ip_address, user_agent: r.user_agent, created_at: date, updated_at: date }
-  end
-}.reject(&:nil?))
+    { message_id: r.message_id, ip_address: $ips.sample, user_agent: $agents.sample, created_at: date, updated_at: date }
+  })
+
+  # open logs
+  ClickLog.create(campaign.tracking_logs.joins(:open_logs).select($prefix + 'tracking_logs.*', $prefix + 'open_logs.*').map{|r|
+    RouletteWheel::with(0.7) do 
+      date = rand(24).hours.ago
+      { message_id: r.message_id, url: $links.sample[:url], ip_address: r.ip_address, user_agent: r.user_agent, created_at: date, updated_at: date }
+    end
+  }.reject(&:nil?))
+  
+  # update campaign status
+  campaign.name = 'Product Weekly Newsletter' if campaign.name == 'Untitled'
+  campaign.status = 'done'
+  campaign.save
+end
